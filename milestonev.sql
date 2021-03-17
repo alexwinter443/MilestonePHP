7600499b96d703285598507a87fc54fd58a3a77c
alexwinter443 <59127575+alexwinter443@users.noreply.github.com>
2021-03-16 23:57:37 -0700

    Add files via upload

---
+++ milestonev.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM tehqysltlkccm9g6.job_posting
+LIMIT 0, 1000
+
+-- Date: 2021-03-16 23:57
+*/
+INSERT INTO `` (`ID`,`title`,`description`,`wage`,`location`,`requirements`,`jobtype`) VALUES (1,'Engineer','Electrical Engineer',25,'Phoenix','Bachelors','full time');
+INSERT INTO `` (`ID`,`title`,`description`,`wage`,`location`,`requirements`,`jobtype`) VALUES (2,'Teacher','Secondary school math teacher',18,'Glendale','Bachelors','full time');
+INSERT INTO `` (`ID`,`title`,`description`,`wage`,`location`,`requirements`,`jobtype`) VALUES (3,'Java Developer','5 year experience',25,'Phoenix','Bachelors','full time');
+INSERT INTO `` (`ID`,`title`,`description`,`wage`,`location`,`requirements`,`jobtype`) VALUES (6,'developer','senior java developer',NULL,NULL,NULL,NULL);
